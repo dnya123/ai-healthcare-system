70e536fbfde4452c7c0728a44055f49c13e47616
Dnyaneshwari <dnyaneshwaridhakulkar@gmail.com>
2026-05-20 17:30:34 +0530

    Implemented MongoDB connection and JWT authentication

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Healthcare API Running");
